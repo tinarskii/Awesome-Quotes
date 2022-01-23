5868afc96372ada602278cf80a7fc2ce8be79bd4
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-01-23 15:29:18 +0700

    Add something idk

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,26 @@
 <script>
    export let quote = {text: '', author: ''};
    
-    export let random = async() => {
-      fetch('https://type.fit/api/quotes')
-      .then(res => res.json())
-      .then(data => {
-        quote = data[Math.floor(Math.random() * data.length)];
-      });
-    }
+   import { page } from '$app/stores'
+   const id = $page.url.searchParams.get('id')
+   
+   export let random = async() => {
+     quote.text = 'Loading...'
+     quote.author = ''
+     fetch('https://type.fit/api/quotes').then(res => res.json()).then(data => {
+       if (id) {
+         if(!data[id]) return;
+         quote.text = data[id].text
+         quote.author = data[id].author
+         quote.id = id
+       } else {
+         let randomNum = Math.floor(Math.random() * data.length);
+         quote.text = data[randomNum].text;
+         quote.author = data[randomNum].author;
+         quote.id = randomNum
+       }
+     });
+   }
     
     random()
 </script>
@@ -21,11 +34,11 @@
             <div class="text-3xl text-center animate-pop container max-w-3xl">
               <span class="before:block before:absolute before:-inset-4 before:bg-blue-500 before:-skew-y-2 before:rounded-lg relative inline-block">
                 <p class="relative text-white">
-                    {quote['text'] || 'Loading...'}
+                  <a href="/?id={quote.id}">{quote['text'] || 'Loading...'}</a>
                 </p>
               </span>
               <p class="text-black mt-8">
-                  {quote['author'] || 'Unknown'}
+                {quote['author'] || 'Unknown'}
               </p>
             </div>
             <center>
@@ -35,4 +48,10 @@
             </center>
         </div>
     </div>
+</div>
+<div class="absolute top-0 left-0 p-2 text-gray-500">
+  By <a href="https://github.com/tinvv">Tinnaphat Somsang (@tinvv)</a>
+</div>
+<div class="absolute top-0 right-0 p-2 text-gray-500">
+  <a href="https://github.com/tinvv/awesome-quotes">Github</a>
 </div>
