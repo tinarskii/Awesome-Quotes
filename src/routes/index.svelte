8ccc9f7d8b0b58a91abdbe2c63d0920470dbfb44
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-01-23 14:36:07 +0700

    Change quotes source

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,15 @@
 <script>
-    import { quotes } from '$lib/quotes';
-
-    export let quote = quotes[Math.floor(Math.random() * quotes.length)];
-
-    export let random = () => {
-        quote = quotes[Math.floor(Math.random() * quotes.length)];
+   export let quote = {text: '', author: ''};
+   
+    export let random = async() => {
+      fetch('https://type.fit/api/quotes')
+      .then(res => res.json())
+      .then(data => {
+        quote = data[Math.floor(Math.random() * data.length)];
+      });
     }
+    
+    random()
 </script>
 
 <div class="grid h-screen place-items-center">
@@ -14,12 +18,15 @@
             <p class="md:text-8xl text-6xl m-4 text-center font-bold animate-bounce">
                 Awesome Quotes
             </p>
-            <div class="text-3xl text-center flex-none animate-pop">
-                <span class="before:block before:absolute before:-inset-4 before:bg-blue-500 before:-skew-y-2 relative inline-block">
-                <span class="relative text-white">
-                    {quote}
-                </span>
-            </span>
+            <div class="text-3xl text-center animate-pop container max-w-3xl">
+              <span class="before:block before:absolute before:-inset-4 before:bg-blue-500 before:-skew-y-2 relative inline-block">
+                <p class="relative text-white">
+                    {quote['text']}
+                </p>
+              </span>
+              <p class="text-black mt-8">
+                  {quote['author'] || 'Unknown'}
+              </p>
             </div>
             <center>
                 <button on:click={() => random()} class="bg-blue-500 text-white px-4 py-2 rounded-lg max-w-sm mt-8 hover:bg-blue-600 active:scale-90 duration-200">
